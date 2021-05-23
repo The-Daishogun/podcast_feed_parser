581bb1347d66c4cddc527845fe1fee91985e6d8c
Mohammadreza Varasteh <varastehmr@gmail.com>
2021-05-23 18:41:41 +0430

    RENAME main file

--- podcast_feed_parser.py
+++ podcast_feed_parser.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 
-class PodcastFeed:
-    itunes_podcast_required_tags_single = [
+class PodcastFeedParser:
+    itunes_podcast_required_tags = [
         "title",
         "description",
         "itunes:image",
@@ -42,13 +42,14 @@ class PodcastFeed:
 
     def __init__(self, feed_url: str) -> None:
         self.feed_url = feed_url
-        self.feed_soup = BeautifulSoup(
-            requests.get(feed_url).content, "xml", from_encoding="utf-8"
+        self.feed_content = requests.get(feed_url).content
+        self.feed_soup: BeautifulSoup = BeautifulSoup(
+            self.feed_content, "xml", from_encoding="utf-8"
         )
 
     @property
     def is_itunes_compatible(self) -> bool:
-        headings = self.soup.findall("rss")
+        headings = self.feed_soup.find_all("rss")
         for heading in headings:
             if (
                 heading["xmlns:itunes"] == "http://www.itunes.com/dtds/podcast-1.0.dtd"
@@ -56,7 +57,14 @@ class PodcastFeed:
             ):
                 return True
         return False
-        
+
     def parse_itunes(self) -> dict:
         if self.is_itunes_compatible:
-
+            parser_results = {
+                key: self.feed_soup.find(key).text
+                for key in self.itunes_podcast_required_tags
+                if key != "category"
+            }
+            print(parser_results)
+        else:
+            return "podcast is not itunes compatible"
