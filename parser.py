e73324b0412189116c742c1d7ecbfe5be28fea68
Mohammadreza Varasteh <varastehmr@gmail.com>
2021-05-23 18:26:08 +0430

    FIX mistake

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 
 
 class PodcastFeed:
@@ -41,7 +42,21 @@ class PodcastFeed:
 
     def __init__(self, feed_url: str) -> None:
         self.feed_url = feed_url
-        self.feed_soup = BeautifulSoup(self.feed_url)
+        self.feed_soup = BeautifulSoup(
+            requests.get(feed_url).content, "xml", from_encoding="utf-8"
+        )
 
-    def parse_itunes(self) -> dict:
+    @property
+    def is_itunes_compatible(self) -> bool:
+        headings = self.soup.findall("rss")
+        for heading in headings:
+            if (
+                heading["xmlns:itunes"] == "http://www.itunes.com/dtds/podcast-1.0.dtd"
+                and heading["version"] == "2.0"
+            ):
+                return True
+        return False
         
+    def parse_itunes(self) -> dict:
+        if self.is_itunes_compatible:
+
